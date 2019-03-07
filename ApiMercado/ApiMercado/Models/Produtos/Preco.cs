60d8cd6320f20e564586800372521d8be1b70934
Eduardo Cardoso <edkardoso@hotmail.com>
2019-03-07 18:30:17 -0300

    start models

---
+++ ApiMercado/ApiMercado/Models/Produtos/Preco.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiMercado.Models.Produtos
+{
+    public class Preco
+    {
+        public string NCM { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime Atualizacao { get; set; }
+        public string Estabelecimento { get; set; }
+    }
+}
---
+++ ApiMercado/ApiMercado/Models/Produtos/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiMercado.Models.Produtos
+{
+    public class Produto
+    {
+        public int Id { get; private set; }
+        public string NCM { get; private set; }
+        public string Descricao { get; private set; }
+        public decimal PrecoAtual { get; private set; }
+
+
+    }
+}
