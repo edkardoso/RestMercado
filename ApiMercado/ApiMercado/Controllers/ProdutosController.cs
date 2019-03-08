2db6250ee15b91984c1e44328e7d025c3b45db05
Eduardo Cardoso <edkardoso@hotmail.com>
2019-03-08 11:24:08 -0300

    iniciando a controller de produtos

--- ApiMercado/ApiMercado/Controllers/ProdutosController.cs
+++ ApiMercado/ApiMercado/Controllers/ProdutosController.cs
@@ -1,41 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiMercado.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class ProdutosController : Controller
     {
-        // GET api/values
+        // GET api/produtos
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET api/produtos/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST api/produtos
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/produtos/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/produtos/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
